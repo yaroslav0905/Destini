b4443c6b6e49a6720d54793ed63081e63e2de924
YaroslavKambarov <yaroslav@rocketfirm.com>
2022-07-06 14:38:05 +0600

    add text and story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoise(1), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoise(2), for: .normal)
+    }
+    
+    let choice1 = "Take a left"
+    let choice2 = "Take a right"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func selectionMade(_ sender: UIButton) {
+        storyBrain.checkAnswer(sender.currentTitle!)
+        updateUI()
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct Story {
+    let text : String
+    let btn1 : String
+    let btn2 : String
+    let destination1 : Int
+    let destination2 : Int
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        text = title
+        btn1 = choice1
+        btn2 = choice2
+        destination1 = choice1Destination
+        destination2 = choice2Destination
+    }
+}
